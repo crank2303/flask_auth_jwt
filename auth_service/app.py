4e9fdce636b2b0b25e4a5a08c2cb5e3f7c603a3b
Mikhail Kraynikov <mkraynikov@house-me.ru>
2023-02-14 07:45:33 +0300

    Base project structure.

---
+++ auth_service/app.py
@@ -0,0 +1,42 @@
+import os
+
+from flask import Flask
+from flask_redoc import Redoc
+from marshmallow import Schema, fields
+
+class PetSchema(Schema):
+    name = fields.Str()
+
+app = Flask(__name__)
+
+app.config['REDOC'] = {
+    'spec_route': '/docs',
+    'title': 'Flask Auth Service',
+    'marshmallow_schemas': [PetSchema]}
+
+redoc = Redoc(app, 'service.yml')
+
+
+@app.route('/')
+def index():
+    return "Hello World!"
+
+
+@app.route('/random')
+def random():
+    """A cute furry animal endpoint.
+    ---
+    get:
+      description: Get a random pet
+      responses:
+        200:
+          description: Return a pet
+          content:
+            application/json:
+              schema: PetSchema
+    """
+    return PetSchema().dump(dict(name="Bird"))
+
+
+if __name__ == "__main__":
+    app.run()
